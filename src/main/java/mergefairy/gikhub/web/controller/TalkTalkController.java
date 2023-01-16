c9066792576bca13322806350359dabcdc01950b
jminkkk <minl741@naver.com>
2023-01-16 13:41:41 +0900

    feat : 게시판 댓글순, 최신순 정렬 기능 추가

--- src/main/java/mergefairy/gikhub/web/controller/TalkTalkController.java
+++ src/main/java/mergefairy/gikhub/web/controller/TalkTalkController.java
@@ -4,18 +4,22 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mergefairy.gikhub.domain.TalkTalk;
 import mergefairy.gikhub.service.Dto.TalkTalkCreateDto;
-import mergefairy.gikhub.service.TalkTalkSeviceImpl;
+import mergefairy.gikhub.service.Dto.TalkTalkGetDto;
+import mergefairy.gikhub.service.TalkTalkServiceImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/talktalk")
 public class TalkTalkController {
-    private final TalkTalkSeviceImpl talkTalkService;
+    private final TalkTalkServiceImpl talkTalkServiceImpl;
 
     //게시글 등록 화면
     @GetMapping("/addForm")
@@ -26,18 +30,38 @@ public class TalkTalkController {
     //게시글 등록
     @PostMapping("/add")
     public ResponseEntity createTalkTalk(@Valid TalkTalkCreateDto talkTalkDto){
-        TalkTalk createdTalkTalk = talkTalkService.createTalkTalk(talkTalkDto);
+        TalkTalk createdTalkTalk = talkTalkServiceImpl.createTalkTalk(talkTalkDto);
         return new ResponseEntity(createdTalkTalk, HttpStatus.CREATED);
     }
 
     //게시글 삭제
-    @DeleteMapping("/delete/{no}")
-    public ResponseEntity deleteTalkTalk(Long no){
-        talkTalkService.deleteTalkTalk(no);
+    @DeleteMapping("/delete/{talkTalkNo}")
+    public ResponseEntity deleteTalkTalk(@PathVariable Long talkTalkNo){
+        talkTalkServiceImpl.deleteTalkTalk(talkTalkNo);
         return new ResponseEntity("{}", HttpStatus.OK);
     }
 
-//    //게시글 목록
-//    @GetMapping("/list")
-//    public
+    //게시판
+    //게시글 1개 조회
+    @GetMapping("/{talkTalkNo}"
+    )
+    public ResponseEntity getOneTalkTalk(@Valid @PathVariable("talkTalkNo") Long no){
+        TalkTalk findTalkTalk = talkTalkServiceImpl.getOneTalkTalk(no);
+        return new ResponseEntity(findTalkTalk, HttpStatus.OK);
+    }
+
+    //게시글 목록 무한 스크롤으로 반환타입은 Slice
+    //댓글순
+    @GetMapping("/list/commentCount/{pageNumber}")
+    public ResponseEntity<Slice<TalkTalkGetDto>> getTalkTalksSortedByCommentCount(@PathVariable int pageNumber) {
+        Slice<TalkTalkGetDto> talkTalksByCommentCount = talkTalkServiceImpl.getTalkTalksSortedByCommentCount(pageNumber);
+        return new ResponseEntity(talkTalksByCommentCount, HttpStatus.OK);
+    }
+
+    //최신순
+    @GetMapping("/list/new/{pageNumber}")
+    public ResponseEntity<Slice<TalkTalkGetDto>> getTalkTalksSortedByCreateAt(@PathVariable int pageNumber) {
+        Slice<TalkTalkGetDto> talkTalksByCreateAt = talkTalkServiceImpl.getTalkTalksSortedByCreateAt(pageNumber);
+        return new ResponseEntity(talkTalksByCreateAt, HttpStatus.OK);
+    }
 }
