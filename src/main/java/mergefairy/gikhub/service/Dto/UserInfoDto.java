fcd0993464a605348f442a6cf5ca976f39f7141f
jminkkk <minl741@naver.com>
2023-01-17 22:11:19 +0900

    feat : 유저 도메인에 실명 속성 추가

--- src/main/java/mergefairy/gikhub/service/Dto/UserInfoDto.java
+++ src/main/java/mergefairy/gikhub/service/Dto/UserInfoDto.java
@@ -1,20 +1,22 @@
 package mergefairy.gikhub.service.Dto;
 
-import jakarta.persistence.Column;
 import lombok.Data;
 import mergefairy.gikhub.domain.User;
 
 @Data
 public class UserInfoDto {
     private String email;
-    private String name;
+    private String realName;
+    private String password;
     private String phoneNo;
     private String nickName;
 
     public UserInfoDto(User user){
         this.email = user.getEmail();
-        this.name = user.getName();
+        this.realName = user.getRealName();
         this.phoneNo = user.getPhoneNo();
+        this.password = user.getPassword();
         this.nickName = user.getNickName();
     }
+
 }
