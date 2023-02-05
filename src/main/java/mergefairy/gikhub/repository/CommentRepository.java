2c662d31ca1ea012654938e5b45f2c491de9c9db
jminkkk <minl741@naver.com>
2023-02-05 18:03:05 +0900

    feat: 댓글 반환

--- src/main/java/mergefairy/gikhub/repository/CommentRepository.java
+++ src/main/java/mergefairy/gikhub/repository/CommentRepository.java
@@ -4,12 +4,19 @@ import mergefairy.gikhub.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Comment save(Comment comment);
-    List<Comment> findAllByTalkTalk(Long talkTalkNo);
 
-    Comment findByTalkTalkNo(Long talkTalkNo);
+    //게시글에 해당하는 댓글 리스트
+    List<Comment> findAllByTalkTalk(long talkTalk);
+
+    //댓글 아이디에 따라 댓글 하나 반환
+    Optional<Comment> findById(Long id);
+
+    //댓글
+    List<Comment> findAllByParent(long id);
 }
