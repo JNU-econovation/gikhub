50b34cfb28549e47577708d56e6405d81c6fabfc
jminkkk <minl741@naver.com>
2023-02-18 17:12:44 +0900

    feat:공지사항 크롤링 구현

--- src/main/java/mergefairy/gikhub/service/HomeServiceImpl.java
+++ src/main/java/mergefairy/gikhub/service/HomeServiceImpl.java
@@ -1,4 +1,15 @@
 package mergefairy.gikhub.service;
 
+import mergefairy.gikhub.service.Dto.noticeDto;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Service
 public class HomeServiceImpl {
+
+    public List<noticeDto> getNotice(){
+        return getNotice();
+    }
 }
--- src/main/java/mergefairy/gikhub/web/controller/HomeController.java
+++ src/main/java/mergefairy/gikhub/web/controller/HomeController.java
@@ -3,24 +3,30 @@ package mergefairy.gikhub.web.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mergefairy.gikhub.domain.User;
+import mergefairy.gikhub.service.Dto.noticeDto;
+import mergefairy.gikhub.service.crawler.Crawler;
 import mergefairy.gikhub.web.session.SessionConst;
-import mergefairy.gikhub.repository.UserRepository;
-import mergefairy.gikhub.web.session.SessionManager;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import java.io.IOException;
+import java.util.List;
+
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/")
 public class HomeController {
-    private final UserRepository userRepository;
-    private final SessionManager sessionManager;
+
 
 
     //로그인 하지 않은 사용자도 홈에 접근할 수 있기 때문에 required = false 를 사용
-    @GetMapping("/")
+    @GetMapping("/login")
     public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User loginUser, Model model){
 
         //세션이 없을때
@@ -29,6 +35,13 @@ public class HomeController {
         //세션이 유지되면 로그인홈으로
         model.addAttribute("user", loginUser);
         return "loginHome";
+    }
+
+    //공지사항
+    @GetMapping("/notice")
+    public ResponseEntity<List<noticeDto>> getNotice() throws IOException {
+        List<noticeDto> notices = Crawler.getNotice();
+        return new ResponseEntity(notices, HttpStatus.OK);
     }
 }
 
